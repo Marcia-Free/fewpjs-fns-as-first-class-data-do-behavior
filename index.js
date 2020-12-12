57425a51f4bcdfae24d6ec520fab12325c7bb19d
Marcia-Free <MarciaFreeman2011@gmail.com>
2020-12-11 21:09:49 -0600

    commit tree f37945f99cebc66609433302c98c53badb8b34ff

--- index.js
+++ index.js
@@ -6,8 +6,29 @@ function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
-
 /* End Given Code, don't edit above here...*/
 
+
+
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let timeArray = timeString.split(':')
+  const hour = parseInt(timeArray[0])
+
+  if (hour < 12) {
+    return "Good Morning"
+  } 
+  else if (hour >= 12 && hour <= 17) {
+    return "Good Afternoon"
+  } 
+  else {
+    return "Good Evening"
+  }
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  let greeting = document.querySelector('#greeting')
+      greeting.innerText = message
+}
